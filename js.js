e17ba2be966a4537c4b14dd62b2a46719a6d349b
rafi090604 <rafi090604@gmail.com>
2023-12-07 23:16:16 +0700

    baru

--- js.js
+++ js.js
@@ -5,8 +5,8 @@ function navigateToPage2() {
     window.location.href = "index2.html";
 }
 function hasilhitung(){
-    var panjang = document.getElementById("datap").value;
-    var tinggi = document.getElementById("datat").value;
+    var panjang = document.getElementById("data-p").value;
+    var tinggi = document.getElementById("data-t").value;
     var intpanjang = parseInt(panjang)
     var inttinggi = parseInt(tinggi)
     document.getElementById("hasil").innerHTML = (intpanjang + inttinggi)/2;
@@ -20,3 +20,21 @@ function hasilhitung2(){
     var intc = parseInt(sisic)
     document.getElementById("hasil2").innerHTML = intc + intb + inta;
 }
+function resetInputBoxh1() {
+    document.getElementById("data-p").value = '';
+    document.getElementById("data-t").value = '';
+    document.getElementById("hasil").innerHTML= '';
+}
+document.getElementById("data-p").document.getElementById("data-t").addEventListener("click", resetInputBoxh1);
+document.getElementById("hasil").innerHTML.addEventListener("click", resetInputBoxh1);
+
+function resetInputBoxh2() {
+    document.getElementById("dataA").value = '';
+    document.getElementById("dataB").value = '';
+    document.getElementById("dataC").value='';
+    document.getElementById("hasil2").innerHTML= '';
+}
+document.getElementById("dataA").document.getElementById("dataB"). document.getElementById("dataC").addEventListener("click", resetInputBoxh2);
+document.getElementById("hasil2").innerHTML.addEventListener("click", resetInputBoxh2);
+
+
